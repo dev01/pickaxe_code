ee44e1fdf6380b4e6f8c37608fde6766912ff10b
d3v <example@example.com>
2012-09-05 22:25:07 +0530

    added indexing feature to SongList class and unit tests

--- ruby_juke.rb
+++ ruby_juke.rb
@@ -50,6 +50,27 @@ end
 
 class SongList
 
+  def initialize
+    @songs = Array.new
+  end
+
+  def append(song)
+    @songs.push(song)
+    self
+  end
+
+  def delete_first
+    @songs.shift
+  end
+
+  def delete_last
+    @songs.pop
+  end
+
+  def [](index)
+    @songs[index]
+  end
+
   MAX_TIME = 5*60        # 5 minutes
 
   def SongList.is_too_long(song)
@@ -67,6 +88,29 @@ class MyLogger
   def MyLogger.create
     @@logger = new unless @@logger
     @@logger
+  end
+
+end
+
+
+require 'test/unit'
+class TestSongList < Test::Unit::TestCase
+  
+  def test_delete
+    list = SongList.new
+    s1 = Song.new('title1', 'artst1', 1)
+    s2 = Song.new('title2', 'artst2', 2)
+    s3 = Song.new('title3', 'artst3', 3)
+    s4 = Song.new('title4', 'artst4', 4)
+    list.append(s1).append(s2).append(s3).append(s4)
+    assert_equal(s1, list[0])
+    assert_equal(s3, list[2])
+    assert_nil(list[9])
+    assert_equal(s1, list.delete_first)
+    assert_equal(s2, list.delete_first)
+    assert_equal(s4, list.delete_last)
+    assert_equal(s3, list.delete_last)
+    assert_nil(list.delete_last)
   end
 
 end
