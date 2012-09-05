03d0855f0cc3032b4db6f5566fc31ae039c2998f
d3v <example@example.com>
2012-09-05 22:52:01 +0530

    added WordIndex class

--- ruby_juke.rb
+++ ruby_juke.rb
@@ -83,6 +83,28 @@ class SongList
 
 end
 
+class WordIndex
+
+  def initialize
+    @index = {}
+  end
+
+  def add_to_index(obj, *phrases)
+    phrases.each do |phrase|
+      phrase.scan(/\w[-w']+/) do |word|      # extract each word
+        word.downcase!
+        @index[word] = [] if @index[word].nil?
+        @index[word].push(obj)
+      end
+    end
+  end
+
+  def lookup(word)
+    @index[word.downcase]
+  end
+
+end
+
 class MyLogger
 
   private_class_method :new
