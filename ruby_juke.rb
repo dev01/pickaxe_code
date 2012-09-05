ccab713eaea38b12a287c60ab63640380c82735c
d3v <example@example.com>
2012-09-05 21:48:03 +0530

    added to_s method for Song class

--- ruby_juke.rb
+++ ruby_juke.rb
@@ -1,9 +1,15 @@
 #!/usr/local/bin/ruby -w
 
 class Song
+
   def initialize(name, artist, duration)
     @name     = name
     @artist   = artist
     @duration = duration
   end
+
+  def to_s
+    "song: #@name--#@artist (#@duration)"
+  end
+
 end
